de756b505817824cc91df56af262575a587fbccb
Sumon Ahmed <bdsumon4u@gmail.com>
2025-06-10 19:05:55 +0600

    feat: update schedule for DistributeSpecialFunds command to run in background and avoid overlapping

--- routes/console.php
+++ routes/console.php
@@ -9,4 +9,8 @@ Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
 
-Schedule::command(DistributeSpecialFunds::class)->monthly();
+Schedule::command(DistributeSpecialFunds::class)
+    ->monthly()
+    ->at('00:01')
+    ->withoutOverlapping()
+    ->runInBackground();
